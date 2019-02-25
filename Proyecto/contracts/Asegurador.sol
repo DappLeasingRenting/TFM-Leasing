6eb03fc31f9154f1271692ee1157d40775bf5af5
DappLeasingRenting <47277058+DappLeasingRenting@users.noreply.github.com>
2019-02-25 21:41:35 +0100

    Add files via upload

--- Proyecto/contracts/Asegurador.sol
+++ Proyecto/contracts/Asegurador.sol
@@ -3,20 +3,57 @@ pragma solidity ^0.4.2;
 
 import "./SafeMath.sol";
 
-contract CompraToken  {
+contract Asegurador  {
     address admin;
-   
-    uint256 public PrecioToken;
-    uint256 public tokensVendidos;
+    
+    uint256 public PrecioKmCiudad;
+    uint256 public PrecioKmCarretera;
+    uint256 public PrecioAparcado;
 
-    event CostoSeguro(idCoche, CostoTotal);
-    event recordCliente(msg.sender, idLicencia, record);
+    event CostoSeguro(uint idCoche, uint TotalCost);
+
+    mapping(uint => uint) public aseguradoraPrecioAparcado;
+    mapping(uint => uint) public aseguradoraPrecioCarretera;
+    mapping(uint => uint) public aseguradoraPrecioCiudad;
+
+    mapping(uint => address) public adminAseguradora;
+    mapping(address => uint) public IdEmpresa;
+
+
+
+    function actualizarPrecioAparcado(uint precioAparcado) public {
+        /*uint idEmpresa;
+        idEmpresa = IdEmpresa[msg.sender];
+        require(adminAseguradora[idEmpresa] == msg.sender);
+        aseguradoraPrecioAparcado[idEmpresa] = precioAparcado;*/
+        aseguradoraPrecioAparcado[0] = precioAparcado;
+    }
+    function actualizarPrecioCarretera(uint precioCarretera) public {
+        uint idEmpresa;
+        idEmpresa = IdEmpresa[msg.sender];
+        require(adminAseguradora[idEmpresa] == msg.sender);
+        aseguradoraPrecioCarretera[idEmpresa] = precioCarretera;
+    }
+    function actualizarPrecioCiudad(uint precioCiudad) public {
+        /*uint idEmpresa;
+        idEmpresa = IdEmpresa[msg.sender];
+        require(adminAseguradora[idEmpresa] == msg.sender);
+        aseguradoraPrecioCiudad[idEmpresa] = precioCiudad;*/
+        aseguradoraPrecioCiudad[0] = precioCiudad;
+    }
 
 
 //** @title Constructor. */
-  
+    
 //** @title Compra de tokens. */
-    function costoSeguro(uint idCoche, uint _tiempoAparcado, uint _tiempoMovimientoCiudad, uint _tiempoMovimientoCarretera, uint record) public {
+    function costoSeguro(uint idCoche, 
+    uint _tiempoAparcado,
+    uint _tiempoMovimientoCiudad,
+    uint _tiempoMovimientoCarretera, 
+    uint record,
+    uint CostoAparcado,
+    uint CostoMovimientoCiudad,
+    uint CostoMovimientoCarretera) public {
           /**@param _numeroTokens cantidad a comprar.
  
       /** @dev verificar que el precio a pagar sea el precio estblecido
@@ -24,67 +61,19 @@ contract CompraToken  {
       verifica que se haya realizado la transacción satisfactoriamente desde el contrato Token.sol.
       Actualiza el valor de los tokens vendidos
       */ 
-        CostoAparcado = SafeMath.mult(_tiempoAparcado, PrecioAparcado);
-        CostoMovimientoCiudad = SafeMath.mult(_tiempoMovimientoCiudad, PrecioKmCiudad);
-        CostoMovimientoCarretera = SafeMath.mult(_tiempoMovimientoCarretera, PrecioKmCarretera);
-        CostoMovimiento = SafeMath.add(CostoMovimientoCiudad, CostoMovimientoCarretera);
-        CostoTotal = safe.mult((SafeMath.add(CostoMovimientoCiudad, CostoMovimientoCarretera)),(safemath.div(1/record)));
+        uint CostoMovimiento;
+        uint CostoTotal;
 
-        /** @return devuelve el addres del que compra y la cantidad*/
+        CostoAparcado = SafeMath.mul(_tiempoAparcado, PrecioAparcado);
+        CostoMovimientoCiudad = SafeMath.mul(_tiempoMovimientoCiudad, PrecioKmCiudad);
+        CostoMovimientoCarretera = SafeMath.mul(_tiempoMovimientoCarretera, PrecioKmCarretera);
+        CostoMovimiento = SafeMath.add(CostoMovimientoCiudad, CostoMovimientoCarretera);
+        CostoTotal = SafeMath.mul((SafeMath.add(CostoMovimientoCiudad, CostoMovimientoCarretera)),(SafeMath.div(1, record)));
+        
         emit CostoSeguro(idCoche, CostoTotal);
     }
 
-    function recordCliente(uint idLicencia, uint antiguedadLicencia, uint puntosLicencia /*Oraculo*/) public returns(uint) {
-        /** @param nombre nombre del usuario.
-            @param idUsuario identifiación del usuario.
-            @param fecha fecha en la que se realiza el alta del usuario.
-        */
-        /** @dev verifica que el Smartcontract este activo
-        verifica que la address no haya creado un usuario ya en la DAAP y finalmente verifica que no se hayan asignado ya todos los 
-        periodos de retiro de Tokens*/
-       require(Activo == false);
-
-        if (antiguedadLicencia <= 3){
-            record = SafeMath.div(puntosLicencia, 8);
-        }else if (antiguedadLicencia >= 6 && antiguedadLicencia >= 9){
-            record = SafeMath.div(puntosLicencia, 12);
-        }else if (antiguedadLicencia >= 9 && antiguedadLicencia <= 12){
-            record = SafeMath.div(puntosLicencia, 14);
-        }else if (antiguedadLicencia > 15){
-            record = SafeMath.div(puntosLicencia, 15);
-        }
-        
-        /**@return emite el evento con los datos de mes, nombre y fecha de comienzoAhorro.*/
-        emit recordCliente(msg.sender, idLicencia, record);
-        
-        
-    }
+    
 
 
-//** @title Depotio de tokens. */
-    function depositoAhorro(uint256 _contribucion, uint fechaEjecucion) public payable {
-        /**@param _contribucion cantidad a ahorrar.
-       @param fechaEjecucion fecha en que se realiza el deposito.*/
-      /** @dev verificar que el balance de quien envía sea mayor a la cantidad a enviar
-      verifica que se haya realizado la transacción satisfactoriamente desde el contrato Token.sol.
-      */ 
-       require(tokenContract.balanceOf(msg.sender) >= _contribucion);
-       require(tokenContract.transferFrom(msg.sender, this, _contribucion, fechaEjecucion));
-       /** @return devuelve el address del que solicita el deposito y la cantidad.*/
-       emit Deposito(msg.sender, _contribucion);
-    }
-//** @title Retiro de tokens. */
-    function retiroTokens(uint256 _retiro, uint fechaEjecucion) public payable {     
-        /**@param _retiro cantidad a retirar.
-       @param fechaEjecucion fecha en que se realiza el retiro.*/
-      /** @dev verificar que el balance del contrato sea mayor al solicitado por quien retira
-      verifica que se haya realizado la transacción satisfactoriamente desde el contrato Token.sol.
-      Actualiza el valor de los tokensVendidos*/ 
-        require(tokenContract.balanceOf(this) >= _retiro);
-        require(tokenContract.transferRetiro(msg.sender, _retiro, fechaEjecucion));
-
-        tokensVendidos += _retiro;
-//** @return devuelve el address del que solicita el retiro y la cantidad.*/
-        emit Retiro(msg.sender, _retiro);
-    }
 }
--- Proyecto/contracts/Cliente.sol
+++ Proyecto/contracts/Cliente.sol
@@ -3,36 +3,38 @@ import "./SafeMath.sol";
 import "./Owned.sol";
 
 
-contract Token {
+contract Cliente is Owned {
 
     bool public Activo = false;
+    uint puntosLicencia = 8;
     
     event nuevoCliente(
-        address clienteId, 
-        uint edad, 
-        uint antiguedadLicencia
-    );
-    event modificar(
-        uint idUsuario,
-        address adUsuario, 
-        uint valorNuevo
+        string idLicencia, 
+        uint antiguedadLicencia,
+        uint record
     );  
+
+    
+    string public tipoSeguroXDefecto = '0A';
+    string public idCocheXDefecto = '0A';
 
     mapping(address => cliente) public clientes;
     mapping(address => uint) public ownerCuentaLeasing;
+    
 
        
-    
    
 
     struct cliente {
-        address clienteId;
+        string nombre;
         uint edad;
-        uint idLicencia;
+        string idLicencia;
         uint AntiguedadLicencia;
-        string tipoSeguro;
+        bool Seguro;
         string idCoche; 
-        bool prestamo;
+        bool credito;
+        uint recordCliente;
+        bool leasing;
     }
 
 
@@ -51,7 +53,7 @@ contract Token {
     }
 
     //** @title Crear Ahorrador. */
-    function _crearCliente(uint edad, uint antiguedadLicencia, string idLicencia) public returns(bool) {
+    function crearCliente(string nombre, uint edad, string idLicencia, uint antiguedadLicencia) public {
         /** @param nombre nombre del usuario.
             @param idUsuario identifiación del usuario.
             @param fecha fecha en la que se realiza el alta del usuario.
@@ -59,19 +61,31 @@ contract Token {
         /** @dev verifica que el Smartcontract este activo
         verifica que la address no haya creado un usuario ya en la DAAP y finalmente verifica que no se hayan asignado ya todos los 
         periodos de retiro de Tokens*/
+
+       uint record;
        require(Activo == false);
-       require((ownerCuentaLeasing[msg.sender]) == 0);
+       //require((ownerCuentaLeasing[msg.sender]) == 0);
+        if (antiguedadLicencia <= 3){
+            record = SafeMath.div(puntosLicencia, 8);
+        }else if (antiguedadLicencia >= 6 && antiguedadLicencia >= 9){
+            record = SafeMath.div(puntosLicencia, 12);
+        }else if (antiguedadLicencia >= 9 && antiguedadLicencia <= 12){
+            record = SafeMath.div(puntosLicencia, 14);
+        }else if (antiguedadLicencia > 15){
+            record = SafeMath.div(puntosLicencia, 15);
+        }
        /** @dev cuando se registra el sexto usuario, se inicializan las fechas para el contrato y para los usuarios, de acuerdo
        a la fecha de registro de último usuario*/             
         /** @dev se da de alta los valores del usuario que se ha registrado para cada uno de lo campos del strcut en el mapping Ahorrado
         Se suma el 1 a los contadores mes y ownerCuenta Ahorro, mesCount y mes*/
-        clientes[msg.sender] = cliente(msg.sender, edad, idLicencia, antiguedadLicencia, 0, 0, false);
+        clientes[msg.sender] = cliente(nombre, edad, idLicencia, antiguedadLicencia, false, idCocheXDefecto, false, record,false);
         ownerCuentaLeasing[msg.sender] ++;
         /**@return emite el evento con los datos de mes, nombre y fecha de comienzoAhorro.*/
-        emit nuevoCliente(edad, idLicencia, antiguedadLicencia);
+        emit nuevoCliente(idLicencia, antiguedadLicencia, record);
         
         
     }
+
 
 
 
