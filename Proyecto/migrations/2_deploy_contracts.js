b48a0b30169598032a9c23123335671becbf72c8
DappLeasingRenting <47277058+DappLeasingRenting@users.noreply.github.com>
2019-02-27 23:30:26 +0100

    Add files via upload

--- Proyecto/migrations/2_deploy_contracts.js
+++ Proyecto/migrations/2_deploy_contracts.js
@@ -1,17 +1,5 @@
-var Token = artifacts.require("./Token.sol");
-var CompraToken = artifacts.require("./CompraToken.sol");
+var Cliente = artifacts.require("./Cliente.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Token, 1000000).then(function() {
-    // Token price is 0.001 Ether
-    var PrecioToken = 1000000000000000;
-    return deployer.deploy(CompraToken, Token.address, PrecioToken);
-  });
+  deployer.deploy(Cliente);  
 };
-
-
-
-
-
-
-
