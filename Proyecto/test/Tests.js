ee216c24d70e220559a7972b510648d468e041ef
Pedro <dappleasingrenting@gmail.com>
2019-04-21 22:22:23 +0200

    Tests finales Pedro

--- Proyecto/test/Tests.js
+++ Proyecto/test/Tests.js
@@ -263,13 +263,14 @@ contract('CompraToken', function (accounts) {
 
 //con el siguiente test ya estamos verificando la función de transfer desde el contrato al admin
 
-  it('Revisión de la compra de Tokens', function () {
+  it('Revisión de la compra de Tokens y transferencia inicial', function () {
         return Token.deployed().then(function (instance) {
           tokensDisponibles=10000;
           TokenInstance = instance;
           return CompraToken.deployed();
         }).then(function (instance) {
           CompraToken = instance;
+         
           return TokenInstance.transfer(CompraToken.address, tokensDisponibles, { from: admin })
         
           //Se realiza el despliegue de la función indicandole los valores requeridos para su debido funcionamiento, e indicando que el ejecutor es la cuenta Admin 
@@ -281,177 +282,18 @@ contract('CompraToken', function (accounts) {
           assert.equal(Compra.logs.length, 1, 'Se registra el evento');
           assert.equal(Compra.logs[0].event, 'Venta', 'Debe generarse el evento Venta');
           assert.equal(Compra.logs[0].args._comprador, admin, 'Se registra el addres de quien realiza la compra');
-          assert.equal(Compra.logs[0].args._tokens, numeroTokens, 'Se muestra en el evento el número de Tokens comprado');
-          return TokenInstance.transferInicial(CompraToken.address, 100, { from: admin })
+          assert.equal(Compra.logs[0].args._tokens, numeroTokens, 'Se muestra en el evento el número de Tokens comprado');    
+          return TokenInstance.transferInicial(admin, 100)
                   }).then(function (Compra) {
           //Se verifica que los datos generado en el evento sean correctos
           assert.equal(Compra.logs.length, 1, 'Se registra el evento');
           assert.equal(Compra.logs[0].event, 'TransferInicial', 'Debe generarse le evento Venta');
-          assert.equal(Compra.logs[0].args._from, admin, 'Se registra el addres de quien realiza la compra');
-          assert.equal(Compra.logs[0].args._to, CompraToken.address, 'destino');
+          assert.equal(Compra.logs[0].args._to, admin, 'destino');
           assert.equal(Compra.logs[0].args._value, 100, 'valor');
+          
      })
       });
 
 
-    //   it('Revisión de la transferencia inicial de Tokens', function () {
-    //     return Token.deployed().then(function (instance) {
-    //       tokensDisponibles=10000;
-    //       TokenInstance = instance; 
-    //       CompraToken = instance;
-    //       return TokenInstance.transferInicial(CompraToken.address, 100, { from: admin })
-    //     }).then(function (Compra) {
-    //       //Se verifica que los datos generado en el evento sean correctos
-    //       assert.equal(Compra.logs.length, 1, 'Se registra el evento');
-    //       assert.equal(Compra.logs[0].event, 'TransferInicial', 'Debe generarse le evento Venta');
-    //       assert.equal(Compra.logs[0].args._from, admin, 'Se registra el addres de quien realiza la compra');
-    //       assert.equal(Compra.logs[0].args._to, CompraToken.address, 'destino');
-    //       assert.equal(Compra.logs[0].args._value, 100, 'valor');
-    //  })
-    //   });
-
-  // it('Verificamos que la compra de tokens es correcta ', function () {
-  //   return CompraToken.deployed().then(function (instance) {
-  //     CompraTokenInstance = instance;
-  //     return CompraTokenInstance.compraTokens(10, { from: admin });
-  //   }).then(function (ResultadoCompra) {
-  //     assert.equal(ResultadoCompra.logs[0].event, 'Venta', 'El evento se ha generado');
-  //     assert.equal(ResultadoCompra.logs[0].args._comprador, admin, 'El address coincide con el del sender');
-  //     assert.equal(ResultadoCompra.logs[0].args._tokens, 10000000000000000, 'El número de tokens es correcto');
-  //   });
-  // });
-
-  // it('Verificamos que la compra de tokens es correcta ', function () {
-  //   return CompraToken.deployed().then(function (instance) {
-  //     CompraTokenInstance = instance;
-  //     //console.log(PrecioToken + " Precio token");
-  //     return CompraTokenInstance.prestamoTokens(10, cuenta4, { from: admin });
-  //   }).then(function (ResultadoCompra) {
-  //     assert.equal(ResultadoCompra.logs[0].event, 'Venta', 'El evento se ha generado');
-  //     assert.equal(ResultadoCompra.logs[0].args._comprador, admin, 'El address coincide con el del sender');
-  //     assert.equal(ResultadoCompra.logs[0].args._tokens, 1000000000000000, 'El address coincide con el del sender');
-  //   });
-  // });
-
-
-
-//   it('Revisión de la compra y Depostito de Tokens', function () {
-//     return Token.deployed().then(function (instance) {
-      
-//       TokenInstance = instance;
-//       return CompraToken.deployed();
-//     }).then(function (instance) {
-//       CompraToken = instance;
-//       return TokenInstance.transfer(CompraToken.address, tokensDisponibles, { from: admin })
-//       //Se realiza el despliegue de la función indicandole los valores requeridos para su debido funcionamiento, e indicando que el ejecutor es la cuenta Admin 
-//     }).then(function (respuestatransfer1) {
-//       numeroTokens = 10;
-//       return CompraToken.compraTokens(numeroTokens, { from: comprador, value: numeroTokens * PrecioToken })
-//     }).then(function (Compra) {
-//       //Se verifica que los datos generado en el evento sean correctos
-//       assert.equal(Compra.logs.length, 1, 'Se registra el evento');
-//       assert.equal(Compra.logs[0].event, 'Venta', 'Debe generarse le evento Venta');
-//       assert.equal(Compra.logs[0].args._comprador, comprador, 'Se registra el addres de quien realiza la compra');
-//       assert.equal(Compra.logs[0].args._tokens, numeroTokens, 'Se muestra en el evento el número de Tokens comprado');
-//       return CompraToken.tokensVendidos();
-//     }).then(function (cantidadTokens) {
-//       assert.equal(cantidadTokens.toNumber(), numeroTokens, 'Se incrementa el número de tokens vendidos');
-//       return TokenInstance.balanceOf(comprador);
-//     }).then(function (balance) {
-//       assert.equal(balance.toNumber(), numeroTokens, 'El número de Tokens del usuario ha cambiado en el valor de tokens comprados');
-//       numeroTokensDeposito = 10;
-//       fechaEjecucionDeposito= 2175806719;
-//       return TokenInstance.transferFrom(CompraToken.address, admin, numeroTokensDeposito, fechaEjecucionDeposito, { from: admin });
-//     }).then(function (respuestatransferFrom) {
-//       return CompraToken.depositoAhorro(numeroTokensDeposito, fechaEjecucionDeposito, { from: admin, value: numeroTokens * PrecioToken })
-//     }).then(function (Deposito) {
-//       assert.equal(Deposito.logs.length, 1, 'Se registra el evento de deposito');
-//       assert.equal(Deposito.logs[0].event, 'Deposito', 'Debe generarse le evento Venta');
-//       assert.equal(Deposito.logs[0].args._depositante, admin, 'Se registra el addres de quien realiza la compra');
-//       assert.equal(Deposito.logs[0].args.cantidad, numeroTokensDeposito, 'Se muestra en el evento el número de Tokens comprado');
-//       numeroTokensRetiro = 10;
-//       fechaEjecucionRetiro = 2175806719;
-// })
-//   });
-
-
-
-//   it('solo permite la creación de máximo 6 usuarios para el ahorro', function () {
-//     return Token.deployed().then(function (instance) {
-//       TokenInstance = instance;
-//       /*Se crea un usuario a través de la cuenta comprador y se realiza una verificación de los datos de alta del mismo*/
-//       TokenInstance._crearAhorrador("Omar", 16308, 1544897465, {from: comprador});
-//       return TokenInstance.Ahorradores(comprador);
-//     }).then(function (Resultado2) {
-//       assert.equal(Resultado2[2], "Omar", 'El contrato devuelve los datos correctos de nombre de usuario');;
-//       assert.equal(Resultado2[1], 16308, 'El contrato devuelve los datos correctos del mes asignado al usuario');
-//       /*Se crea un usuario a través de la cuenta cuenta1 y se realiza una verificación de los datos de alta del mismo*/
-//       TokenInstance._crearAhorrador("Omar", 16308, 1544897465, {from: cuenta1});
-//       return TokenInstance.Ahorradores(cuenta1);
-//     }).then(function (Resultado3) {
-//       assert.equal(Resultado3[2], "Omar", 'El contrato devuelve los datos correctos de nombre de usuario');;
-//       assert.equal(Resultado3[1], 16308, 'El contrato devuelve los datos correctos del mes asignado al usuario');
-//        /*Se crea un usuario a través de la cuenta cuenta2 y se realiza una verificación de los datos de alta del mismo*/
-//       TokenInstance._crearAhorrador("Omar", 16308, 1544897465, {from: cuenta2});     
-//       return TokenInstance.Ahorradores(cuenta2);
-//      }).then(function (Resultado4) {
-//       assert.equal(Resultado4[2], "Omar", 'El contrato devuelve los datos correctos de nombre de usuario');;
-//       assert.equal(Resultado4[1], 16308, 'El contrato devuelve los datos correctos del mes asignado al usuario');
-//       /*Se crea un usuario a través de la cuenta cuenta3 y se realiza una verificación de los datos de alta del mismo*/
-//       TokenInstance._crearAhorrador("Omar", 16308, 1544897465, {from: cuenta3});      
-//       return TokenInstance.Ahorradores(cuenta3);
-//      }).then(function (Resultado5) {
-//       assert.equal(Resultado5[2], "Omar", 'El contrato devuelve los datos correctos de nombre de usuario');;
-//       assert.equal(Resultado5[1], 16308, 'El contrato devuelve los datos correctos del mes asignado al usuario');
-//       /*Se crea un usuario a través de la cuenta cuenta4 y se realiza una verificación de los datos de alta del mismo*/
-//       TokenInstance._crearAhorrador("Omar", 16308, 1544897465, {from: cuenta4});      
-//       return TokenInstance.Ahorradores(cuenta4);
-//      }).then(function (Resultado6) {
-//       assert.equal(Resultado6[2], "Omar", 'El contrato devuelve los datos correctos de nombre de usuario');;
-//       assert.equal(Resultado6[1], 16308, 'El contrato devuelve los datos correctos del mes asignado al usuario');
-//       /*Se crea un  usuario a través de la cuenta cuenta5 y dado que sería el septimo usuario se realiza una verificación 
-//       del error que se debe generar*/
-//       return TokenInstance._crearAhorrador("Omar", 16308, 1544897465, {from: cuenta5});      
-//      }).then(assert.fail).catch(function (error) {
-//       assert(error.message.indexOf('revert') >= 0, 'no se pueden crear mas de 6 cuentas');
-//   })
-//     });
-
-//   it('solo permite la creación de un usuario por Address', function () {
-//     return Token.deployed().then(function (instance) {
-//       TokenInstance = instance;
-//       /*Se intenta crear un segundo usuario a través de la cuenta admin que en la prueba de verificación de los datos
-//       de alta ya se había realizado cuentas ya había creado una*/
-//       return TokenInstance._crearAhorrador("Omar", 16308, 1544897465,{ from: admin});
-//     }).then(assert.fail).catch(function (error) {
-//       assert(error.message.indexOf('revert') >= 0, 'msg.sender solo puede tener un registro');
-//     })
-//   });
-//     it('solo permite el owner pueda realizar la transferencia incial al contracto', function () {
-//     return Token.deployed().then(function (instance) {
-//         TokenInstance = instance;
-//         //se hace la llamada de la fucnión para la transferencia inicial contrato desde una función que no es la Owner(admin)
-//     return TokenInstance.transferInicial(CompraToken.address, tokensDisponibles, { from: cuenta1 })
-//   }).then(assert.fail).catch(function (error) {
-//       assert(error.message.indexOf('revert') >= 0, 'msg.sender solo puede tener un registro');
-//   })
-// })
-// it('solo permite el owner pueda realizar detener el contracto', function () {
-//   return Token.deployed().then(function (instance) {
-//       TokenInstance = instance;
-//   return TokenInstance.DetenerContratoToken({ from: cuenta1 })//se hace la llamada de la fucnión desactivar contrato desde una función que no es la Owner
-// }).then(assert.fail).catch(function (error) {
-//     assert(error.message.indexOf('revert') >= 0, 'msg.sender solo puede tener un registro');
-// })
-// })
-
-// it('solo permite el owner pueda realizar detener el contracto', function () {
-//   return Token.deployed().then(function (instance) {
-//       TokenInstance = instance;
-//   return TokenInstance.ActivarContratoToken({ from: cuenta1 })//se hace la llamada de la función activar contrato desde una función que no es la Owner
-// }).then(assert.fail).catch(function (error) {
-//     assert(error.message.indexOf('revert') >= 0, 'msg.sender solo puede tener un registro');
-// })
-// })
 
 })
